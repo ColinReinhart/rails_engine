36fbe2618cf66d31299e517977173f9e05e6b972
Colin Reinhart <colinbreinhartgmail.com>
2022-07-21 14:02:36 -0600

    Bullshit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+
   namespace :api do
     namespace :v1 do
+      namespace :revenue do
+        resources :merchants, only: [:index]
+      end
       get '/items/find_all', to: 'items#find_all'
       get '/items/find', to: 'items#find'
       get '/merchants/find', to: 'merchants#find'
